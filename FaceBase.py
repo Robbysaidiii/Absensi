7df326a1a8c935e13e65c37a428ce91567789757
Robby Saidi Prasetyo <157108492+Robbysaidiii@users.noreply.github.com>
2024-12-06 17:19:24 +0700

    Update FaceBase.py

--- FaceBase.py
+++ FaceBase.py
@@ -11,7 +11,7 @@ def connect_to_database():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="", 
+        password="",
         database="facebase"
     )
 
